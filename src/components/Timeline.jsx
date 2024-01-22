e7dad6e7624cf72c2a5fcd6ada2452b91088cbe0
Jahir <jahirmj2001@gmail.com>
2024-01-22 11:34:31 +0530

    Add Media query for 14 inch laptop

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -20,11 +20,10 @@ const Timeline = () => {
 
 const TimelineItem = ({ heading, text, index }) => (
   <div
-    className={`timelineItem ${
-      index % 2 === 0 ? "leftTimeline" : "rightTimeline"
-    }`}
+    className={`timelineItem ${index % 2 === 0 ? "leftTimeline" : "rightTimeline"
+      }`}
   >
-    <div>
+    <div className="timelineContainer">
       <h2>{heading}</h2>
       <p>{text}</p>
     </div>
